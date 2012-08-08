e54790dea6b7433837d757c1c0e3fb82d97d55d5
Jeen Lee <aiatejin@gmail.com>
2012-08-08 15:54:13 +0900

    initial commit

---
+++ lib/App/Zamakist.pm
@@ -0,0 +1,32 @@
+package App::Zamakist;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+App::Zamakist -
+
+=head1 SYNOPSIS
+
+  use App::Zamakist;
+
+=head1 DESCRIPTION
+
+App::Zamakist is
+
+=head1 AUTHOR
+
+Jeen Lee E<lt>aiatejin@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
